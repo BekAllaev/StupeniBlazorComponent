2ceee20ac0d09b63aab98cde1d278adb481bc08f
Bekzod Allaev <allaev_@outlook.com>
2024-02-28 21:43:43 +0200

    Add first prototype of StupeniCard and its test

---
+++ ComponentsCollection/Card/IStupeniCard.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentsCollection.Card;
+
+/// <summary>
+/// Card which have title, content and button in the right top corner
+/// </summary>
+public interface IStupeniCard
+{
+    [Parameter]
+    public string? Icon { get; set; }
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public string? ButtonText { get; set; }
+
+    [Parameter]
+    public Color ButtonColor { get; set; }
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public bool IsButtonDisabled { get; set; }
+
+}
